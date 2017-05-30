6af36b10fdbc2a880dbe9bd1001c23ff202e0d11
Jagadeesh <jagadeesh962@gmail.com>
2017-05-30 12:19:48 +0530

    firstname and lastname added to the user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   	get :download_pdf, on: :collection
   	get :download_csv, on: :collection
   end
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'transactions#index'
   get 'transactions/new'
   get 'transactions/show'
